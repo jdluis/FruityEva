2be7ce2236007408df8e8b0ec3d079ac068be57e
jdluis <92221396+jdluis@users.noreply.github.com>
2021-12-01 15:27:04 +0000

    resources

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: { //se pondria la etiqueta que usa tailwind-default
+        primary:'#FF6363', //aqui la nueva declaracion que queremos incorporarle
+        secondary: {
+          100:'#E2E2D5', 
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        textPrimary ['Ubuntu'],
+        textPrimary ['Lobster']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
