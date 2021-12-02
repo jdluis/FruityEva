355a197790045e215550f18f93700437f690fdf5
jdluis <92221396+jdluis@users.noreply.github.com>
2021-12-02 13:35:31 +0000

    Testing tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,18 @@ module.exports = {
   theme: {
     extend: {
       colors: { //se pondria la etiqueta que usa tailwind-default
-        primary:'#FF6363', //aqui la nueva declaracion que queremos incorporarle
+        primary: '#FF6363',
         secondary: {
-          100:'#E2E2D5', 
-          200: '#888883',
-        }
+          100:'#E2E2D5',
+          200:'#888883',
+        },
+        palette: {
+          one:'#830A1E',
+          two:'#DE2647',
+          three:'#F3593B',
+          four:'#F19B3E',
+          five:'#F3BD62',
+        },
       },
       fontFamily: {
         textPrimary: ['Ubuntu'],
