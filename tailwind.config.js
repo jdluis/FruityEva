73bb2f209d6b558d8aafa7515112a9423f6d6964
jdluis <92221396+jdluis@users.noreply.github.com>
2021-12-06 22:42:41 +0000

    Home page
    Not ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: { //se pondria la etiqueta que usa tailwind-default
-        primary: '#FF6363',
         secondary: {
           100:'#E2E2D5',
           200:'#888883',
@@ -16,13 +15,23 @@ module.exports = {
           four:'#F19B3E',
           five:'#F3BD62',
         },
+        palette2: {
+          one:'#F0EFE9',
+          two:'#F19381',
+          three:'#F1B9A7',
+          four:'#60AA9C',
+          five:'#77C7DE',
+          six:'#B38FB0',
+          text:'#5B5E67'
+        },
+
       },
       fontFamily: {
         textPrimary: ['Sahitya'],
         textSecondary: ['Ubuntu']
       },
       fontSize: {
-        h1: ['14rem']
+        h1: ['12.5rem']
       },
       backgroundImage: {
         'hero-home': "url('https://i.giphy.com/media/XEb90OzfzTduM/giphy.webp')",
