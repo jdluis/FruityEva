0e46d41576f5a82163227f519ec1769f36f6e936
jdluis <92221396+jdluis@users.noreply.github.com>
2021-12-09 19:57:56 +0000

    add style of form section, add imgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,6 +36,7 @@ module.exports = {
       backgroundImage: {
         'hero-home': "url('https://i.giphy.com/media/XEb90OzfzTduM/giphy.webp')",
         'orange': "url(https://i.giphy.com/media/k7K48NJ7qlko8/giphy.webp)",
+        'home2section': "url(img/bg2sec.jpg);)",
       },
     },
   },
